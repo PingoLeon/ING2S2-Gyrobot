3f9e96b830cbe714aec1b4b29ac254ab565c519c
Léon <dalleleon@gmail.com>
2024-05-13 16:31:27 +0200

    ET1

--- src/main.cpp
+++ src/main.cpp
@@ -1,40 +1,34 @@
-#include <Arduino.h>
-#include <Wire.h>
 #include <Adafruit_MPU6050.h>
 #include <Adafruit_Sensor.h>
+#include <Wire.h>
 
 Adafruit_MPU6050 mpu;
 
+
+//Capter l'angle de tangeage à partir de l'accéléromètre en Y et Z
+float AccelerometerData() {
+  sensors_event_t a, g, temp;
+  mpu.getEvent(&a, &g, &temp);
+  float angle = (atan2(a.acceleration.y, a.acceleration.z) * 180 / PI) - 90; // l'angle varie de -90 à 90 degrés, 0 étant la position stable
+  Serial.print("Angle: "); Serial.println(angle);
+  return angle;
+}
+
+// Fonction setup
 void setup() {
   Serial.begin(9600);
-  while (!Serial);
-
   if (!mpu.begin()) {
     Serial.println("Failed to find MPU6050 chip");
     while (1) {
       delay(10);
     }
   }
-  mpu.setAccelerometerRange(MPU6050_RANGE_2_G);
+  mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
   mpu.setGyroRange(MPU6050_RANGE_500_DEG);
-  mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
-  Serial.println("MPU6050 is ready!");
-  delay(1000);
+  mpu.setFilterBandwidth(MPU6050_BAND_5_HZ);
 }
 
+// Fonction loop
 void loop() {
-  sensors_event_t a, g, temp;
-  mpu.getEvent(&a, &g, &temp);
-  Serial.print("\n\n\n\n\n\n\n\n\n\n\n\n\n\nAcceleration - X=");
-  Serial.print(a.acceleration.x);
-  Serial.print(" / Y=");
-  Serial.print(a.acceleration.y);
-  Serial.print(" / Z=");
-  Serial.print(a.acceleration.z);
-  //Serial.print("\nGyro - X=");
-  //Serial.print(g.gyro.x);
-  //Serial.print(", Y=");
-  //Serial.print(g.gyro.y);
-  //Serial.print(", Z=");
-  //Serial.println(g.gyro.z);
+  AccelerometerData();
 }
